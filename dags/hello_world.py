7f1bc9fcad588744935729a5cf8b1435f451a1fc
Alejandro Ferrera <alejandro.ferrera@klog.co>
2026-03-20 16:19:02 -0300

    asd

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -36,7 +36,20 @@ def hello_world():
         pick = random.choice(emojis)
         print(f"Today's emoji: {pick}")
 
-    hello() >> random_number() >> log_timestamp() >> random_emoji()
+    # Print a random motivational quote
+    @task
+    def random_quote():
+        import random
+        quotes = [
+            "The best time to plant a tree was 20 years ago. The second best time is now.",
+            "Done is better than perfect.",
+            "Keep it simple, stupid.",
+            "First, solve the problem. Then, write the code.",
+            "Talk is cheap. Show me the code.",
+        ]
+        print(f"Quote of the day: {random.choice(quotes)}")
+
+    hello() >> random_number() >> log_timestamp() >> random_emoji() >> random_quote()
 
 
 hello_world()
